2c92ae1f5b26cc5e443b4c27b2c33dd22fb51d10
Nikkita Tarrao <nikkita.tarrao19@my.stjohns.edu>
2025-12-08 21:16:12 -0500

    added a readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Nikkita Tarrao Personal Website
+This repository contains all the necessary elements to build my very beautiful website. This project has taken me a very long time to complete as I kept on putting it off for years. So I hope you enjoy the outcome. 
+
+```bash
+URL: https://nikkitarrao.github.io/me/
+```
+
+## Website Overview
--- quizLogic.js
+++ quizLogic.js
@@ -1,141 +1,150 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   //Rendering Initial View
   backEndRestAPI("questionsQ1", 1, "#initialScreen");
 
 
-//event delegation for views
-document.querySelector('#display-data').addEventListener('click', (e) => {
-  handleViewEvents(e);
-});
+  //event delegation for views
+  document.querySelector('#display-data').addEventListener('click', (e) => {
+    handleViewEvents(e);
+  });
 
 
-return false;
+  return false;
 }); //end of DOMContentLoaded 
 
 
-
+let qid = 0;
 let counter = 0;
+let answers = [];
 
-handleViewEvents =  (e) => {
+handleViewEvents = (e) => {
   if (e.target.type !== 'radio') {
-   e.preventDefault();
+    e.preventDefault();
   }
   if (e.target.dataset.viewaction == "startQuiz") {
-  //saving user entered name
-  console.log(e.target.dataset.viewaction);
-  var name = document.querySelector('#name').value; 
-  console.log(name);
-  let qid = 1;
+    console.log(e.target.dataset.viewaction);
+    qid = 1;
     //entering quiz 1
-    if((document.querySelector('#quiz-selection').value === "1")){
+    if ((document.querySelector('#quiz-selection').value === "1")) {
       quizId = "questionsQ1";
       console.log(quizId);
+      console.log(qid);
       backEndRestAPI(quizId, qid, "#quiz_view");
     }
     //entering quiz 2
-    else if(document.querySelector('#quiz-selection').value === "2"){
+    else if (document.querySelector('#quiz-selection').value === "2") {
       quizId = "questionsQ2";
       console.log(quizId);
       backEndRestAPI(quizId, qid, "#quiz_view");
-    } 
+    }
     //entering quiz 3
-    else if(document.querySelector('#quiz-selection').value === "3"){
+    else if (document.querySelector('#quiz-selection').value === "3") {
       quizId = "questionsQ3";
       console.log(quizId);
       backEndRestAPI(quizId, qid, "#quiz_view");
-    } 
+    }
     //entering quiz 4
-    else if(document.querySelector('#quiz-selection').value === "4"){
+    else if (document.querySelector('#quiz-selection').value === "4") {
       quizId = "questionsQ4";
       console.log(quizId);
       backEndRestAPI(quizId, qid, "#quiz_view");
-    } 
+    }
 
     return false;
   }
 
-//handling each question
-console.log(qid);
-console.log(quizId);
-if(e.target.dataset.viewaction == "continue"){ 
-  qid++;
-  backEndRestAPI(quizId, qid, "#quiz_view");
-  console.log(qid);
-  console.log(counter);
-}
-//end of test screen
-//choice 1
-else if(e.target.dataset.viewaction == "continue" && qid > 5 && counter/5 >= 0.8){
-  backEndRestAPI(quizId, qid, "#finalScreenPassed");
-  console.log(counter);
-  onsole.log(counter/5);
-  document.querySelector('#name').innerHTML = name;
-}
-else if(e.target.dataset.viewaction == "continue" && qid > 5 && counter/5 >= 0.8){
-//choice 2
-}
-else if(e.target.dataset.viewaction == "continue" && qid > 5 && counter/5 >= 0.8){
-//choice 3
-}
-else if(e.target.dataset.viewaction == "continue" && qid > 5 && counter/5 >= 0.8){
-  //choice 4
-}
+  //handling each question
+  console.log("qid: " + qid);
+  if (e.target.dataset.viewaction == "nextQuestion") {
+    qid++;
 
 
 
-//return button
-if (e.target.dataset.viewaction == "return") {
-  qid = 0;
-  backEndRestAPI("questionsQ1", 1, "#initialScreen");
-}
-
-//re-take
-if (e.target.dataset.viewaction == "re-take") {
-  qid = 1;
-  backEndRestAPI(quizId, 1, "#quiz_view");
-}
-
-//saving values for the test (multiple choice and image questions)
-if (e.target.type == 'radio') {
-  if(e.target.value == document.querySelector('#form').dataset.correctChoice){
-    rightAnswer = true;
-    console.log(e.target.value);
-    console.log(document.querySelector('#form').dataset.correctChoice);
-    console.log(rightAnswer);
+    backEndRestAPI(quizId, qid, "#quiz_view");
+    console.log(qid);
   }
-  else{
-    rightAnswer = false;
-    console.log(e.target.value);
-    console.log(document.querySelector('#form').dataset.correctChoice);
-    console.log(rightAnswer);
+  //end of test screen
+  //choice 1
+  else if (e.target.dataset.viewaction == "nextQuestion" && qid > 5 && counter / 5 >= 0.8) {
+    backEndRestAPI(quizId, qid, "#finalScreenPassed");
+    console.log(counter);
+    onsole.log(counter / 5);
+    // document.querySelector('#name').innerHTML = name;
   }
-}
+  else if (e.target.dataset.viewaction == "nextQuestion" && qid > 5 && counter / 5 >= 0.8) {
+    backEndRestAPI(quizId, qid, "#finalScreenFailed");
+  }
+  else if (e.target.dataset.viewaction == "nextQuestion" && qid > 5 && counter === 0) {
+    backEndRestAPI(quizId, qid, "#finalScreen");
+  }
 
 
-return false;
+
+  //return button
+  if (e.target.dataset.viewaction == "return") {
+    qid = 0;
+    backEndRestAPI("questionsQ1", 1, "#initialScreen");
+  }
+
+  //re-take
+  if (e.target.dataset.viewaction == "re-take") {
+    qid = 1;
+    backEndRestAPI(quizId, 1, "#quiz_view");
+  }
+
+  //saving values for the test (multiple choice and image questions)
+  if (e.target.type == 'radio') {
+    const form = document.querySelector('#form');
+    if ('correctChoice' in form.dataset) {
+      if (e.target.value == document.querySelector('#form').dataset.correctChoice) {
+        rightAnswer = true;
+        counter++;
+        console.log(e.target.value);
+        console.log(document.querySelector('#form').dataset.correctChoice);
+        console.log(rightAnswer);
+      }
+      else {
+        rightAnswer = false;
+        console.log(e.target.value);
+        console.log(document.querySelector('#form').dataset.correctChoice);
+        console.log(rightAnswer);
+      }
+    } else {
+        answers.push(e.target.value);
+    }
+  }
+
+
+  return false;
 } //end of handleViewsEvent
 
 
 //Asynchronous Network Request
-async function backEndRestAPI(quizId,qid, view){
-  let api_endpoint = `https://my-json-server.typicode.com/nikkitarrao/me/tree/ascii-attempt/${quizId}/${qid}`
-  const response = await fetch(api_endpoint)
-  const data = await response.json()
-  console.log(data)
-  console.log(view)
-  const html_element = renderView(data, view)
+async function backEndRestAPI(quizId, qid, view) {
+
+  let api_endpoint = `https://raw.githubusercontent.com/nikkitarrao/me/main/db.json`
+
+  const response = await fetch(api_endpoint);
+  const data = await response.json();
+  console.log("data:", data[quizId])
+  console.log("view: " + view)
+
+  const group = data[quizId];
+  const question = group.find(q => q.id === qid);
+
+  const html_element = renderView(question, view)
   document.querySelector('#display-data').innerHTML = html_element;
-    }
+}
 
- 
 
-  //Rendering View and Update DOM
-  const renderView = (data, view) => {
-      source = document.querySelector(view).innerHTML;
-      var template = Handlebars.compile(source);
-      var html = template(data);
-      return html;
+
+//Rendering View and Update DOM
+const renderView = (data, view) => {
+  source = document.querySelector(view).innerHTML;
+  var template = Handlebars.compile(source);
+  var html = template(data);
+  return html;
 }
 
 //making the cursor special
